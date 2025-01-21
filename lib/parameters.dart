9c7070016481d3aebee52e81b55a041d496344f7
frstuln <udarilakmini98@gmail.com>
2025-01-21 12:50:08 +0530

    added rootkit sdk

--- lib/parameters.dart
+++ lib/parameters.dart
@@ -16,17 +16,14 @@ class Env {
   static String appName =
       "MyApp${Stage.isProd(appStage) ? "" : (Stage.isUat(appStage) ? " - UAT" : " - DEV")}";
   static int timeoutPreSeconds =
-  Stage.isProd(appStage) ? 150 : (kDebugMode ? 120 : 150);
+      Stage.isProd(appStage) ? 150 : (kDebugMode ? 120 : 150);
   static int timeoutWaitSeconds = 30;
 
   static int otpLength = 6;
-
 }
 
 class Constants {
   static const String encoding = "application/octet-stream";
-  static const String justPayConsent =
-      "I hereby authorize to debit this bank account not exceeding the value of the transaction amount (inclusive of the convenience fees as applicable) indicated during a transaction. Further understand that should the debtor be someone other than myself, will not be concerned or required to inquire whether the debtor's name on the record of the party to be credited is the same as that herein stated by me.";
   static String publicKey = Stage.isProd(Env.appStage)
       ? ""
       : "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyeEtnbbx6TKQFaX3yqWKFmCAKfaNb4txC4d34XDxKxzulxPbSVe9jWA0Ygo7S9+jIONP/GyKIA+x8NXz3XrxoDvv+zN28xD6WUXXix2S+l/2b9rEjVd16VjWHFUzspZl8zXUaVwZ6y+rUnQ1T8B0dBQ9hQYdrxanBF78ruLVPLc0OUfELSshHYbAqQfKZU+cDajrXqDCzsJnThjGpGMJFonPtMybKD71ubbguIGU4t1kFO9LcjRkIQclOFYT6Tjv/d3A8biFpRGFAGlcC9Y0Olp+L2hfbAi8KqBVy/jZJm126muKOBJamgZnAGFCu49PZHzBROEjU9upMXXWCok9FQIDAQAB";
@@ -35,7 +32,6 @@ class Constants {
       : ["30c10a56ee852abd7b986a1775319b66dd78ab009062c78282955c37d10317af"];
   static String hmacSecret = Stage.isProd(Env.appStage)
       ? ""
-  // : "g4n7247cg56rv51fv2vstbem71pzn2l2b9vkwqv65ezq5o44bj0dknfxugunap12";
       : "tR0YSPUH01V7OFlZDNMO67opxPF6DUKI5Hkl922sRsGj7HCIJpK8W8ssY7Qap0fC";
 }
 
@@ -43,10 +39,8 @@ class ApiUrl {
   static String baseUrl = Stage.isProd(Env.appStage)
       ? ""
       : (Stage.isDev(Env.appStage)
-      ? "https://testpay.upay.lk/api/"
-      : (Stage.isUat(Env.appStage)
-      ? "https://testpayqa.upay.lk/api/"
-      : "http://192.168.8.104:8080/api/"));
+          ? ""
+          : (Stage.isUat(Env.appStage) ? "" : ""));
 
   static String authService = "auth/";
   static String appConfigService = "config/";
@@ -87,5 +81,4 @@ class ApiUrl {
   static String getPointsBalance = "${baseUrl + authService}user/point-balance";
 
   static String getDeviceList = "${baseUrl + authService}user/device-list";
-
 }
